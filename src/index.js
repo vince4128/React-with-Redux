fa6e2c4dbaffae81d695d7dbc8a133de0a0e0dbc
vince4128 <vrlt.vincent@gmail.com>
2017-06-30 21:12:17 +0200

    app component imported and rendered

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import App from './components/app';
+
 ReactDOM.render(
-  React.createElement('div'), document.querySelector('.container')
+  <App />, document.querySelector('.container')
 );
