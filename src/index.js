9e9ff69d7e9283287cfc0e6cb1bc6c9f67f78d02
vince4128 <vrlt.vincent@gmail.com>
2017-06-30 20:48:48 +0200

    imports added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+ReactDOM.render(
+  React.createElement('div'), document.querySelector('.container')
+);
