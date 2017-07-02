4c9c1049a8b0b7e0273b7cc64b943ea08775459d
vince4128 <vrlt.vincent@gmail.com>
2017-07-02 17:23:49 +0200

    api key added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+//api key
+const API_KEY = 'AIzaSyD4UktRSzQVmt_yYtEDAM6ePRIMgCvX4Kg';
+
 import App from './components/app';
 
 ReactDOM.render(
