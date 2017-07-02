2b93089cb3a661292220aa6a86a64f36f62e8f83
vince4128 <vrlt.vincent@gmail.com>
2017-07-02 17:51:29 +0200

    event handler added

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange={event => console.log(event.target.value)}/>;
   }
 }
 
