f1cb56e62f70360ef32d6720c99da96c19cecbdd
vince4128 <vrlt.vincent@gmail.com>
2017-07-09 16:21:33 +0200

    video-list conponent added

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import SearchBar from './search_bar';
+import VideoList from './video_list';
 
 const App = () => {
   return (
     <div>
       <SearchBar />
+      <VideoList />
     </div>
   );
 }
