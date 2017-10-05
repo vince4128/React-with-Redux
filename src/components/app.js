03a48b9c1f1e97f78cba8a10e838946fe8356194
vince4128 <vrlt.vincent@gmail.com>
2017-10-05 14:09:01 +0200

    onVideoSelect function added and passed as a callback to video_list_item component

--- src/components/app.js
+++ src/components/app.js
@@ -14,11 +14,17 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
 
     //exemple search
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({ 
+        videos:videos,
+        selectedVideo: videos[0]
+      });
       //this.setState({ videos: videos }); es6 condence ({ videos })
     });
 
@@ -28,8 +34,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}//update app states
+        videos={this.state.videos}/>
       </div>
     );
   }
