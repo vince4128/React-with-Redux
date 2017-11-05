69007f2ff118f72e8f35af0732058424831dcdb9
vince4128 <vrlt.vincent@gmail.com>
2017-11-05 14:25:23 +0100

    prevent default

--- weather/src/containers/search_bar.js
+++ weather/src/containers/search_bar.js
@@ -15,9 +15,14 @@ export default class SearchBar extends Component {
         this.setState({ term: event.target.value});
     }
 
+    onFormSubmit(event){
+        event.preventDefault();
+        
+    }
+
     render(){
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group">
                 <input 
                     className="form-control"
                     placeholder="prévision météo pour vos villes préférées."
