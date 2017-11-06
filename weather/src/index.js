b6161e257d96d7e7dce4de998427d8962a6ec83f
vince4128 <vrlt.vincent@gmail.com>
2017-11-06 17:36:28 +0100

    createStoreWithMiddleware & Provider imported

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from 'redux';
 
 import ReduxPromise from 'redux-promise';
 
 import App from "./components/app";
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
     <App />,
