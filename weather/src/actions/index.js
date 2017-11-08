fef06838adf970fd273ac397adc1619964b005ce
vince4128 <vrlt.vincent@gmail.com>
2017-11-08 14:27:02 +0100

    root_url added for the request

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '94af72d0388ec3a71ddb0bf2bdc230dd';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
