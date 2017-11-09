9e10e3049ba1c1be5ba68a461b1e4ab51f39b7e5
vince4128 <vrlt.vincent@gmail.com>
2017-11-09 17:50:19 +0100

    chart and google map components added

--- weather/src/containers/weather_list.js
+++ weather/src/containers/weather_list.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_maps';
 
 class WeatherList extends Component {
 
     renderWeather(cityData){
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
-        console.log(temps);
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        const {lon, lat} = cityData.city.coord;
 
         return(
             <tr key={name}>
-                <td>{name}</td>
-                <td>
-                    <Sparklines height={120} width={180} data={temps}>
-                        <SparklinesLine color="red" />
-                    </Sparklines>
-                </td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
+                <td><Chart data={temps} color="orange" units="C°"/></td>
+                <td><Chart data={pressures} color="green" units="hPa"/></td>
+                <td><Chart data={humidities} color="black" units="%"/></td>
             </tr>
         );
     }
@@ -27,9 +28,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>Ville</th>
-                        <th>Température</th>
-                        <th>Pression</th>
-                        <th>Humidité</th>
+                        <th>Température (C°)</th>
+                        <th>Pression (hPa)</th>
+                        <th>Humidité (%)</th>
                     </tr>
                 </thead>
                 <tbody>
