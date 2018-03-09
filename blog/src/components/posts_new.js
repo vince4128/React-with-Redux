db2465f82ffbfea183afeaa529ed72ae80422cc4
vince4128 <vrlt.vincent@gmail.com>
2018-03-09 15:59:33 +0100

    form submit and errors added

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -2,18 +2,81 @@ import React,  { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-    render() {
-        return(
-            <form>
-                <Field
-                    name="title"
-                    component={}
+
+    renderField(field) {//param field contain some event handlers to wire up to the jsx that we're returning
+        return (
+            <div className="form-group has-danger">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
                 />
+                {field.meta.touched ? field.meta.error : ''}
+            </div>
+        );
+    }
+
+    renderTagsField(field){
+        return(
+            <div className="form-group">
+
+            </div>
+        );
+    }
+
+    onSubmit(values) {
+        //this === component
+        console.log(values);
+    }
+
+    render() {
+
+        const { handleSubmit } = this.props;
+
+        return(
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         )
     }
 }
 
+function validate(values){//helper function
+    const errors = {};
+
+    //validate the inputs from 'values'
+    if(!values.title){
+        errors.title = "Enter a title !";
+    }
+    if(!values.categories){
+        errors.categories = "Enter a categories !";
+    }
+    if(!values.content){
+        errors.content = "Enter a content !";
+    }
+
+    //if errors is empty, the form is fine to submit
+    //if errors as any property, redux form assume form is invalid
+    return errors;
+}
+
 export default reduxForm({
-    form:'PostsNewForm'   //name must be unique, and could be whatever string we want. 
+    validate:validate,
+    form:'PostsNewForm'   //name must be unique (in case of several form it's usefull), and could be whatever string we want. 
 })(PostsNew);
