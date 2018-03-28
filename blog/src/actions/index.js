8b17953e0d6f5b37e2ced94718eb07b38e0ddece
vince4128 <vrlt.vincent@gmail.com>
2018-03-28 13:55:25 +0200

    uri correction

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -32,7 +32,7 @@ export function createPost(values, callback){
 }
 
 export function fetchPost(id) {
-    const request = axios.get(`${ROOT_URL}/posts/${id}`);
+    const request = axios.get(`${ROOT_URL}/${id}`);
 
     return {
         type: FETCH_POST,
