9124a3aa185e1b6088017102c661ee52b6a4bb7b
vince4128 <vrlt.vincent@gmail.com>
2018-03-28 13:56:21 +0200

    post component connected to redux & state mapped to props

--- blog/src/components/posts_show.js
+++ blog/src/components/posts_show.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component{
+
+    componentDidMount(){
+        const { id } = this.props.match.params;
+        this.props.fetchPost(id);
+    }
+
+    helperFunction() {
+        this.props.posts[this.props.match.params.id];
+    }
+
     render(){
+        const { post } = this.props;
+
+        if (!post) {
+            return <div>Loading...</div>;
+          }
+
         return(
             <div>
-                Posts Show !
+                <h3>{post.title}</h3>
+                <h6>Categories : {post.category}</h6>
+                <p>{post.body}</p>  
             </div>
         )
     }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps){//ownProps === component props
+    return { post:posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
